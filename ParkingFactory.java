d4ae2951fa52c131c9c4376337a9841dd6a34d8a
4bcxyz <4bcxyz@gmail.com>
2014-02-21 08:07:42 +0100

    ParkingFactory reads vehicles.dat.
    It create an hashtable which contains the vehicles' list. Vehicles are
    indexed by their name.

--- ParkingFactory.java
+++ ParkingFactory.java
@@ -29,9 +29,28 @@ public class ParkingFactory
       "conf/5-grandmaster.cfg"
    };
 
-   private Hashtable<String, Vehicule> lsVehicule;
+   private Hashtable<String, Vehicule> lsVehicles;
 
    private ParkingFactory ()
    {
+      lsVehicles = new Hashtable<String, Vehicule>();
+      readVehicDat();
+      System.out.println(lsVehicles.size());
+   }
+
+   private void readVehicDat ()
+   {
+      LineFileReader file = new LineFileReader(PATH_VEHIC_DAT);
+      file.open();
+      String line = file.readLine();
+      while (line != null)
+      {
+         String name = line.substring(0, 1);
+         int size = Integer.parseInt(line.substring(1, 2));
+         String col = line.substring(2);
+         lsVehicles.put(name, new Vehicule(name, col, size, 0, 0, "0"));
+         line = file.readLine();
+      }
+      file.close();
    }
 }
